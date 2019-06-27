8a3ce83464df45a38bb145b07f49c12847fe19af
mattig94 <mattig94@users.noreply.github.com>
2019-06-27 16:01:52 -0400

    mark tallest pokemon

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ var repository = [
 	{name: 'Pelipper', height: 47, types: ['water', 'flying']},
 	{name: 'Spheal', height: 31, types: ['ice', 'water']},
 	{name: 'Starmie', height: 43, types: ['water', 'psychic']}
-]
+];
+
 
 for (var i = 0; i < repository.length; i++) {
-	document.write(repository[i].name + ' \(height\: ' + repository[i].height + '\)<br>')
+	document.write(repository[i].name + ' \(height\: ' + repository[i].height + '\)')
+	if (repository[i].height > 45) {
+		document.write(' -Wow, that\'s big!');
+	}
 }
