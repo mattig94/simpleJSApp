edd405fd19af7c33328354aca6d73bceca452c37
mattig94 <mattig94@users.noreply.github.com>
2019-06-27 15:28:16 -0400

    Initialized pokemon repository

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-alert('Hello World!');
+var repository = [
+	{name: 'Pelipper', height: 47, types: ['water', 'flying']},
+	{name: 'Spheal', height: 31, types: ['ice', 'water']},
+	{name: 'Starmie', height: 43, types: ['water', 'psychic']}
+]
