4bc142e0c2b03e0a19bdf9bf4adc84d2ddb8aff7
mattig94 <mattig94@users.noreply.github.com>
2019-07-01 17:38:53 -0400

    refacor code to follow functional programming

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
-var repository = [
-	{name: 'Pelipper', height: 47, types: ['water', 'flying']},
-	{name: 'Spheal', height: 31, types: ['ice', 'water']},
-	{name: 'Starmie', height: 43, types: ['water', 'psychic']}
-];
+var pokemonRepository = (function () {
+	var repository = [
+		{name: 'Pelipper', height: 47, types: ['water', 'flying']},
+		{name: 'Spheal', height: 31, types: ['ice', 'water']},
+		{name: 'Starmie', height: 43, types: ['water', 'psychic']}
+	];
+
+	function getAll(){
+		return repository;
+	}
+
+	function add(pokemon) {
+		repository.push(pokemon);
+	}
+
+	return {
+		add: add,
+		getAll: getAll
+	}
 
 
-// for (var i = 0; i < repository.length; i++) {
-// 	var name = repository[i].name;
-// 	var height = repository[i].height;
-// 	document.write('<p>' + name + ' \(height\: ' + height + '\)')
-// 	if (height > 45) {
-// 		document.write(' -Wow, that\'s big!</p>');
-// 	} else {
-// 		document.write('</p>');
-// 	}
-// };
+}) ();
 
-repository.forEach(function(pokemon){
+pokemonRepository.getAll().forEach(function(pokemon){
 	document.write('<p>' + pokemon.name + ' \(height\: ' + pokemon.height + '\)')
 	if (pokemon.height > 45) {
 		document.write(' -Wow, that\'s big!</p>');
