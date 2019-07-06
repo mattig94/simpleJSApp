1014b880d24bcafd0e8e26f3d62cf61101d5bd8e
mattig94 <mattig94@users.noreply.github.com>
2019-07-06 14:13:01 -0400

    integrate pokedex api, fetch polyfill, and promise polyfill

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 //IIFE
 var pokemonRepository = (function() {
-	var repository = [
-		{ name: 'Pelipper', height: 47, types: ['water', 'flying'] },
-		{ name: 'Spheal', height: 31, types: ['ice', 'water'] },
-		{ name: 'Starmie', height: 43, types: ['water', 'psychic'] }
-	];
+	var repository = [];
+	var apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
 	function getAll() {
 		return repository;
@@ -22,24 +19,69 @@ var pokemonRepository = (function() {
 		nameButton.classList.add('main-pokemon-button');
 		listItem.appendChild(nameButton);
 		$pokemonList.appendChild(listItem);
-		nameButton.addEventListener('click', function(event){
+		nameButton.addEventListener('click', function(event) {
 			showDetails(pokemon);
 		});
 	}
 
-	function showDetails(pokemon){
-		console.log(pokemon.name);
+	function showDetails(item) {
+		pokemonRepository.loadDetails(item).then(function() {
+			console.log(item);
+		});
+	}
+
+	function loadList() {
+		return fetch(apiUrl, { method: 'GET' })
+			.then(function(response) {
+				return response.json();
+			})
+			.then(function(json) {
+				json.results.forEach(function(item) {
+					var pokemon = {
+						name: item.name,
+						detailsUrl: item.url
+					};
+					add(pokemon);
+				});
+			})
+			.catch(function(e) {
+				console.error(e);
+			});
+	}
+
+	function loadDetails(item) {
+		var url = item.detailsUrl;
+		return fetch(url)
+			.then(function(response) {
+				return response.json();
+			})
+			.then(function(details) {
+				// Now we add the details to the item
+				item.imageUrl = details.sprites.front_default;
+				item.height = details.height;
+				item.types = Object.keys(details.types);
+			})
+			.catch(function(e) {
+				console.error(e);
+			});
 	}
 
 	return {
 		add: add,
 		getAll: getAll,
 		addListItem: addListItem,
+		loadList: loadList,
+		loadDetails: loadDetails
 	};
-
 })();
 
 pokemonRepository.getAll().forEach(function(pokemon) {
 	pokemonRepository.addListItem(pokemon);
 });
 
+pokemonRepository.loadList().then(function() {
+	// Now the data is loaded!
+	pokemonRepository.getAll().forEach(function(pokemon) {
+		pokemonRepository.addListItem(pokemon);
+	});
+});
