23d12a93a50bf4111d8b35069873ea3f4a67a34c
mattig94 <mattig94@users.noreply.github.com>
2019-07-09 13:14:06 -0400

    creating modal

--- js/script.js
+++ js/script.js
@@ -26,9 +26,66 @@ var pokemonRepository = (function() {
 
 	function showDetails(item) {
 		pokemonRepository.loadDetails(item).then(function() {
-			console.log(item);
+			createDetails(item);
 		});
 	}
+
+	//create modal
+	function createDetails(item) {
+		//variables
+		var $modalContainer = document.querySelector('#modal-container');
+		var modal = document.createElement('div');
+		var closeButton = document.createElement('button');
+		var imgElement = document.createElement('img');
+		var nameTitle = document.createElement('h2');
+		var heightElement = document.createElement('p');
+		var typeElement = document.createElement('p');
+
+		//clear contents
+		$modalContainer.innerHTML = '';
+
+		//create contents
+		modal.classList.add('modal');
+		closeButton.classList.add('modal-close');
+		closeButton.innerText = 'close';
+			//click event to hide modal
+			closeButton.addEventListener('click', hideModal);
+		imgElement.setAttribute('src', item.imageUrl);
+		imgElement.setAttribute('alt', 'Picture of ' + item.name);
+		nameTitle.innerText = item.name;
+		heightElement.innerText = 'Height: ' + item.height;
+		//typeElement.innerText = 'Types: ' + item.types;
+
+		//add contents
+		modal.appendChild(closeButton);
+		modal.appendChild(imgElement);
+		modal.appendChild(nameTitle);
+		modal.appendChild(heightElement);
+		//modal.appendChild(typeElement);
+		$modalContainer.appendChild(modal);
+
+		$modalContainer.classList.add('is-visible');
+	}
+
+	//basic function to hide modal
+	function hideModal() {
+		document.querySelector('#modal-container').classList.remove('is-visible');
+	}
+
+	//hide modal on keypress
+	window.addEventListener('keydown', (e) => {
+		if(e.key === 'Escape' && document.querySelector('#modal-container').classList.contains('is-visible')) {
+			hideModal();
+		}
+	});
+
+	//hide modal by clicking outside container
+	document.querySelector('#modal-container').addEventListener('click', (e) => {
+		var target = e.target;
+		if (target === document.querySelector('#modal-container')) {
+			hideModal();
+		}
+	});
 
 	function loadList() {
 		return fetch(apiUrl, { method: 'GET' })
